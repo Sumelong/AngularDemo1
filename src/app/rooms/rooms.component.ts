2be1e4c39262d6e7067e81700bff626387063583
Sumelong <130513866+Sumelong@users.noreply.github.com>
2023-12-13 07:38:03 +0100

    inital commit
    Angular Learn demo v1 initail commit

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lnv-rooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css'
+})
+export class RoomsComponent {
+
+}
