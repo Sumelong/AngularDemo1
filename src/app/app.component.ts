d6d3dd166bc6f852963d023b3768305779b8a189
Sumelong <130513866+Sumelong@users.noreply.github.com>
2023-12-13 08:13:08 +0100

    second commit
    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
  import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+ import {RoomsComponent} from "./rooms/rooms.component";
 
 @Component({
   selector: 'lnv-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RoomsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Learnv1';
-  learner ="Frank";
+  hotelName ="Hilton Hotel";
 }
